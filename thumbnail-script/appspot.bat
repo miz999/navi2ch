ff5b4aa6d8fa19c86302b86f9bfd2481ab0086fe
MIZUNUMA Yuto <mizmiz@sf.net>
2020-10-05 00:24:41 +0900

    appspot.bat curl bugfix

--- thumbnail-script/appspot.bat
+++ thumbnail-script/appspot.bat
@@ -4,11 +4,9 @@ setlocal enabledelayedexpansion
 mkdir %~dp5 > NUL 2>&1
 
 if exist "c:/Program Files/Git/mingw64/bin/curl.exe" (
-"c:/Program Files/Git/mingw64/bin/curl.exe" -s "%1?url=%2&w=%3&h=%4" --create-dirs -o "%5"
-"c:/Program Files/Git/mingw64/bin/curl.exe" -s "%1?url=%2&w=%3&h=%4" --create-dirs -o "%5" --dump-header "%5".header
+"c:/Program Files/Git/mingw64/bin/curl.exe" -s --create-dirs --dump-header "%5".header "%1?url=%2&w=%3&h=%4" --create-dirs -o "%5" 
 ) else (
-rem curl -s "%1" --create-dirs -o %2
-curl.exe -s "%1?url=%2&w=%3&h=%4" --create-dirs -o "%5"
+curl.exe -s --create-dirs --dump-header "%5".header "%1?url=%2&w=%3&h=%4" --create-dirs -o "%5" 
 )
 
 if %~z5 EQU 0 (
@@ -16,8 +14,6 @@ if %~z5 EQU 0 (
    echo zero file del !delfile!
    set escaped_delfile=!delfile:/=\\!
    del !escaped_delfile!
-rem echo zero file
-rem echo zero file !escaped_delfile!
 ) else (
   echo done %2
 )
